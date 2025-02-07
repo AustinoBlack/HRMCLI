128c86a21b360b6a279a64dbbdb1ec00a009b292
Austin Black <102131707+AustinoBlack@users.noreply.github.com>
2025-02-06 20:19:47 -0500

    rough definition of setpoweron, running first real test.

--- pcscli/pcs_action.py
+++ pcscli/pcs_action.py
@@ -1,3 +1,5 @@
+import subprocess
+
 def execute_command(prefix, command, node_index):
     """
     Executes a command based on the provided prefix, command, and arguments.
@@ -36,6 +38,16 @@ def execute_command(prefix, command, node_index):
 
 def setpoweron(node_index):
     print(f"Executing setpoweron on node {node_index}")
+    ipmi_command = f"ipmitool -I lanplus -H 172.16.0.2 -U ADMIN -P SJCUKYTBYI power on"
+    
+    try:
+        result = subprocess.run(ipmi_command, shell=True, capture_output=True, text=True)
+        if result.returncode == 0:
+            print(f"Node {node_index} powered on successfully.")
+        else:
+            print(f"Error powering on node {node_index}: {result.stderr}")
+    except Exception as e:
+        print(f"Exception occurred: {str(e)}")
 
 def setpoweroff(node_index):
     print(f"Executing setpoweroff on node {node_index}")
